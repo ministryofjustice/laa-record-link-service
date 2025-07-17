5f0652f8b16d65f306a26c8edf571b2660698aff
Dibyatanu Deb <dibyatanu.deb1@justice.gov.uk>
2025-07-17 13:53:47 +0100

    commented test-report for coverage

--- src/test/java/uk/gov/justice/record/link/service/ApplicationTest.java
+++ src/test/java/uk/gov/justice/record/link/service/ApplicationTest.java
@@ -1,10 +1,10 @@
 package uk.gov.justice.record.link.service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Javadoc comment.
  */
 @SpringBootTest
-public class ApplicationTest { }
+public class ApplicationTest {
+}
