f243b07e28523032383441386c5d0de916bcfd84
Dibyatanu Deb <dibyatanu.deb1@justice.gov.uk>
2025-07-17 13:29:53 +0100

    address code review comments

---
+++ src/test/java/uk/gov/justice/record/link/service/ApplicationTest.java
@@ -0,0 +1,12 @@
+package uk.gov.justice.record.link.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
