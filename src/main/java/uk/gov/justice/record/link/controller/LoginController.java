81684ff22169cfba241b47d31431ab629a638f03
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-07-25 10:02:23 +0100

    STB-2281 User can review request

--- src/main/java/uk/gov/justice/record/link/controller/LoginController.java
+++ src/main/java/uk/gov/justice/record/link/controller/LoginController.java
@@ -1,7 +1,14 @@
 package uk.gov.justice.record.link.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import uk.gov.justice.record.link.model.TransferUser;
+
+import java.util.Objects;
 
 /**
  * Controller for handling login-related requests.
@@ -15,7 +22,19 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String link() {
+    public String transferRequest(Model model, HttpSession session) {
+        TransferUser user = (TransferUser) session.getAttribute("user");
+        if (Objects.isNull(user)) {
+            user = new TransferUser();
+        }
+        model.addAttribute("user", user);
         return "link-user";
+    }
+
+    @PostMapping("/check-answers")
+    public String addUserCheckAnswers(@ModelAttribute TransferUser user, Model model, HttpSession session) {
+        model.addAttribute("user", user);
+        session.setAttribute("user", user);
+        return "check-answers";
     }
 }
