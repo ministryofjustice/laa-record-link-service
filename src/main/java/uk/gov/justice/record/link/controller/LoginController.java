b6e259d58f4eba14d9cdec2b5c32fc65edfc031e
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-07-29 02:17:29 +0100

    Revert "STB-2281 User can review request"
    This reverts commit eab2410a2af93c33dda1bf0b6ca0f4882b261083.

--- src/main/java/uk/gov/justice/record/link/controller/LoginController.java
+++ src/main/java/uk/gov/justice/record/link/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import uk.gov.justice.record.link.model.UserTransferRequest;
+import uk.gov.justice.record.link.model.TransferUser;
 
 import java.util.Objects;
 
@@ -23,16 +23,16 @@ public class LoginController {
 
     @GetMapping("/login")
     public String transferRequest(Model model, HttpSession session) {
-        UserTransferRequest user = (UserTransferRequest) session.getAttribute("user");
+        TransferUser user = (TransferUser) session.getAttribute("user");
         if (Objects.isNull(user)) {
-            user = new UserTransferRequest();
+            user = new TransferUser();
         }
         model.addAttribute("user", user);
         return "link-user";
     }
 
     @PostMapping("/check-answers")
-    public String addUserCheckAnswers(@ModelAttribute UserTransferRequest user, Model model, HttpSession session) {
+    public String addUserCheckAnswers(@ModelAttribute TransferUser user, Model model, HttpSession session) {
         model.addAttribute("user", user);
         session.setAttribute("user", user);
         return "check-answers";
