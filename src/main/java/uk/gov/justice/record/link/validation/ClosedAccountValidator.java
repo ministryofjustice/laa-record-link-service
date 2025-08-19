c370ff1c76b4426c89b07d6cc1d4c03613bfefd1
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-08-19 21:18:43 +0100

    stb-2594-auto reject with closed accounts

--- src/main/java/uk/gov/justice/record/link/validation/ClosedAccountValidator.java
+++ src/main/java/uk/gov/justice/record/link/validation/ClosedAccountValidator.java
@@ -3,12 +3,21 @@ package uk.gov.justice.record.link.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.AllArgsConstructor;
+import uk.gov.justice.record.link.entity.CcmsUser;
+import uk.gov.justice.record.link.respository.CcmsUserRepository;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 public class ClosedAccountValidator implements ConstraintValidator<ValidClosedAccount, String> {
 
+    private static final String CLOSED_EMAIL = "ONLINE-SUPPORT@LEGALAID.GSI.GOV.UK";
+    private final CcmsUserRepository ccmsUserRepository;
+
     @Override
     public boolean isValid(final String oldLogin, final ConstraintValidatorContext context) {
-        return !oldLogin.equalsIgnoreCase("ONLINE-SUPPORT@LEGALAID.GSI.GOV.UK");
+
+        Optional<CcmsUser> user = ccmsUserRepository.findByLoginId(oldLogin);
+        return user.isPresent() && !user.get().getEmail().equalsIgnoreCase(CLOSED_EMAIL);
     }
 }
