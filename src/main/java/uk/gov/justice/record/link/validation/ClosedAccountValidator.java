5e8c6552107a54b1b7ecf6ca3f6a6b988d938597
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-08-19 09:25:43 +0100

    stb-2594- initial changes without test

--- src/main/java/uk/gov/justice/record/link/validation/ClosedAccountValidator.java
+++ src/main/java/uk/gov/justice/record/link/validation/ClosedAccountValidator.java
@@ -3,14 +3,12 @@ package uk.gov.justice.record.link.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.AllArgsConstructor;
-import uk.gov.justice.record.link.respository.CcmsUserRepository;
 
 @AllArgsConstructor
-public final class LoginIdValidator implements ConstraintValidator<ValidLoginId, String> {
-    private final CcmsUserRepository ccmsUserRepository;
+public class ClosedAccountValidator implements ConstraintValidator<ValidClosedAccount, String> {
 
     @Override
     public boolean isValid(final String oldLogin, final ConstraintValidatorContext context) {
-        return ccmsUserRepository.findByLoginId(oldLogin).isPresent();
+        return !oldLogin.equalsIgnoreCase("ONLINE-SUPPORT@LEGALAID.GSI.GOV.UK");
     }
 }
