1b1a8d703c8fde44a21b27073567d8efd5ac0e20
DeborahOjomo <33031521+deborahOjomo@users.noreply.github.com>
2025-08-29 10:02:57 +0100

    STB-2148
    updated the silas constants

--- src/main/java/uk/gov/justice/record/link/constants/SilasConstants.java
+++ src/main/java/uk/gov/justice/record/link/constants/SilasConstants.java
@@ -2,8 +2,9 @@ package uk.gov.justice.record.link.constants;
 
 public class SilasConstants {
     public static final String APP_ROLES = "LAA_APP_ROLES";
-    public static final String USER_NAME = "USER_NAME";
+    public static final String SILAS_LOGIN_ID = "USER_NAME";
     public static final String USER_EMAIL = "USER_EMAIL";
-    public static final String ACCOUNTS = "LAA_ACCOUNTS";
+    public static final String FIRM_CODE = "LAA_ACCOUNTS";
     public static final String FIRM_NAME = "FIRM_NAME";
+    public static final String FULL_NAME = "name";
 }
