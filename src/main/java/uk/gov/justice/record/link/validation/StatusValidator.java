f11cd7bcc9f7a1b3d96fc5bd17516cafcfbb4b1b
sankavi-moj <sankavi.mohanraj@justice.gov.uk>
2025-09-25 10:07:26 +0100

    STB-2492 Generic error message for existing open request

--- src/main/java/uk/gov/justice/record/link/validation/StatusValidator.java
+++ src/main/java/uk/gov/justice/record/link/validation/StatusValidator.java
@@ -4,6 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import uk.gov.justice.record.link.entity.Status;
 import uk.gov.justice.record.link.respository.LinkedRequestRepository;
 
 import java.util.List;
@@ -26,6 +27,7 @@ public class StatusValidator implements ConstraintValidator<ValidStatus, String>
     private Boolean isOfValidStatus(final String oldLogin) {
         final int count = linkedRequestRepository
                 .countByCcmsUser_LoginIdAndStatusIn(oldLogin, List.of(uk.gov.justice.record.link.entity.Status.OPEN, uk.gov.justice.record.link.entity.Status.APPROVED));
-        return count >= 1 ? Boolean.FALSE : Boolean.TRUE;
+        final int requestAlreadyOpened = linkedRequestRepository.countByOldLoginIdAndStatus(oldLogin, Status.OPEN);
+        return count >= 1 || requestAlreadyOpened >= 1 ? Boolean.FALSE : Boolean.TRUE;
     }
 }
